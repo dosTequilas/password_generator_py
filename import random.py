c65b3ddee451afc2bdab78d41778fc913d2ca102
Ferdinand Lange <ferdinandlange@Ferdinands-Laptop-2.local>
2024-06-22 18:16:52 -0400

    initial commit

---
+++ import random.py
@@ -0,0 +1,16 @@
+import random
+
+# initialize the pseudorandom number generator
+random.seed()
+
+print("Password Generator")
+#  write your code below
+password_length = int(input("How many characters would you like your password to be? "))
+password = ''
+counter = 0
+
+while counter < password_length:
+    random_char = chr(random.randrange(48, 123))
+    password += random_char
+    counter += 1
+print(password)
